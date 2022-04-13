b2e728861cc17091bc6b8257873f9d230ec66f1b
Paulo Valente Maia <paulovalentemaia>
2022-04-13 20:09:33 -0300

    bootstrap ui install

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,8 +12,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
     <!-- CSS Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- CSS da App   -->
     <link rel="stylesheet" href="/css/styles.css">
